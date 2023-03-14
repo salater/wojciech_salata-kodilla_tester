6561c803be1221cafd0ee97a8ff88b3fdee0bd8b
Wojciech Salata <mgr.salater@gmail.com>
2023-03-14 13:55:14 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	kodilla-intro/src/main/java/CalcUserDialogs.java

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
@@ -1,11 +1,42 @@
 package com.kodilla.collections.arrays.homework;
 
+
 import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Kia;
+import com.kodilla.collections.interfaces.homework.Opel;
 
 public class CarUtils {
-                                           //public static void describeCar(Car car){
-                                           //}
-    public static void displayCarInfo(Car car) {
 
+    public static void displayCar(Car car) {
+
+        System.out.println("Model of Car: " + getModelCar(car) + "speed: " + car.getSpeed());
+    }
+
+    public static String getModelCar(Car car) {
+        if (car instanceof Ford)
+            return "Ford";
+        else if (car instanceof Kia)
+            return "Kia";
+        else if (car instanceof Opel)
+            return "Opel";
+        else
+            return "Unknown Car name";
+    }
+    public static void getIncreaspeed() {
+        Car autospeed = new Car() {
+            @Override
+            public int getSpeed() {
+                return 0;
+            }
+            @Override
+            public void increaseSpeed() {
+            }
+            @Override
+            public void decreaseSpeed() {
+            }
+        }
     }
 }
+
+
--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
@@ -2,38 +2,41 @@ package com.kodilla.collections.arrays.homework;
 
 import com.kodilla.collections.interfaces.homework.Car;
 import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Kia;
+import com.kodilla.collections.interfaces.homework.Opel;
 
 import java.util.Random;
 
 public class CarsApplication {
-    private String color;
-    private int numberOfDoors;
-    private String name;
-    private int speed;
 
-    public void Car(String color, int numberOfDoors, String name, int speed){
-        this.color = color;
-        this.numberOfDoors = numberOfDoors;
-        this.name = name;
-        this.speed = speed;
+    public static Car drawCar() {
+        // metoda tworzy losowy samochód z losowym przyspieszeniem
+        // losuje samochody jakie ? te co miałem w zadaniu z wyscigiem ?
+        // przyspieszenie to poprostu random liczby np od 10-100 ?
+
+        Random random = new Random();    // wylosowano samochody
+        int whatCar = random.nextInt(3);
+        //double a = getRandomSize();
+        if (whatCar == 0)
+            return new Ford(random.nextInt(), 3, 5);
+        else if (whatCar == 1)
+            return new Kia(200, 2, 4);
+        else {
+            return new Opel(150, 1, 3);
+        }
     }
+
     public static void main(String[] args) {
         Random random = new Random();
         Car[] cars = new Car[random.nextInt(15) + 1];
         for (int n = 0; n < cars.length; n++)
             cars[n] = drawCar();
         for (Car car : cars)
-            CarUtils.displayCarInfo(car);
+            //System.out.println(car);
+            CarUtils.displayCar(car);
+
 
     }
 
-    public static Car drawCar() {
-                                         //Car ford = new Ford(2, 3, 5);
-                                         //return ford;
-    Random random = new Random();
-    int drawnCarKind = random.nextInt(3);
-    int f = drawCar().increaseSpeed(random);
-    if (drawnCarKind == 0)
-        return new Ford(f)
-    }
 }
+
